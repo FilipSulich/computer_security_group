8fe4f027ad6e68f82c005004e0867c6c2c74f1bb
Kanilok <kammotyl@gmail.com>
2025-11-17 22:42:45 +0100

    changed one line back

--- tests/policy_tests.py
+++ tests/policy_tests.py
@@ -11,7 +11,7 @@ server_dir = os.path.join(os.path.dirname(__file__), '..', 'server')
 os.chdir(server_dir)
 sys.path.insert(0, server_dir)
 
-from server.policy import AccessControl
+from policy import AccessControl
 
 @pytest.fixture
 def ac():
